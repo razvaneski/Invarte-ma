444dc900f96598a245c120ff832052e8e0905b55
Radu Paun <radu.paun@mready.net>
2022-05-05 13:20:22 +0300

    Initial Commit

---
+++ dice-swiftui/dice_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  dice_swiftuiApp.swift
+//  dice-swiftui
+//
+//  Created by Radu Paun on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct dice_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
